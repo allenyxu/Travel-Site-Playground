f8a55b5168e7518103b4ddf2024f1f25c2c164e8
Allen Xu <allenyxu@users.noreply.github.com>
2020-11-18 13:32:27 -0800

    Added portiolfio

--- main.py
+++ main.py
@@ -5,29 +5,27 @@ from flask import Flask, render_template
 #create a Flask instance
 app = Flask(__name__)
 
-#connects default URL of server to render home.html
-@app.route('/')
-def home_route():
-  return render_template("home.html", projects=data.setup())
-
 #connects /hello path of server to render hello.html
 @app.route('/beach/')
-def hello_route():
+def beach_route():
   return render_template("beach.html", projects=data.setup())
 
 #connects /flask path of server to render flask.html
 @app.route('/park/')
-def flask_route():
+def park_route():
   return render_template("park.html", projects=data.setup())
 
 @app.route('/restaurant/')
-def playlist_route():
+def restaurant_route():
   return render_template("restaurant.html", projects=data.setup())
 
-@app.route('/videos/')
-def video_route():
-  return render_template("video.html", projects=data.setup())
+@app.route('/')
+def home_route():
+  return render_template("home.html", projects=data.setup())
 
+@app.route('/travel/')
+def travelhome_route():
+  return render_template("travelhome.html", projects=data.setup())
 
 if __name__ == "__main__":
   #runs the application on the repl development server
